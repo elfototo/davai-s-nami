8a4082dbb18ec5381610716e78f2d581dc5feebe
elfototo <e89617881302@gmail.com>
2025-03-11 14:41:59 +0700

    remove hook  useSwipeBack

--- hooks/useSwipeBack.js
+++ hooks/useSwipeBack.js
@@ -1,32 +1,67 @@
-'use client';
+// 'use client';
 
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+// import { useRouter } from "next/navigation";
+// import { useEffect, useState } from "react";
 
-export function useSwipeBack() {
-  const router = useRouter();
-  const [touchStartX, setTouchStartX] = useState(0);
-  const [touchEndX, setTouchEndX] = useState(0);
+// export function useSwipeBack() {
+//   const router = useRouter();
+//   const [touchStartX, setTouchStartX] = useState(0);
+//   const [touchEndX, setTouchEndX] = useState(0);
 
-  useEffect(() => {
-    const handleTouchStart = (e) => {
-      setTouchStartX(e.touches[0].clientX);
-    };
+//   useEffect(() => {
+//     const handleTouchStart = (e) => {
+//       setTouchStartX(e.touches[0].clientX);
+//     };
 
-    const handleTouchEnd = (e) => {
-      setTouchEndX(e.changedTouches[0].clientX);
+//     const handleTouchEnd = (e) => {
+//       setTouchEndX(e.changedTouches[0].clientX);
 
-      if (touchStartX - touchEndX > 50) {
-        router.back();
-      }
-    };
+//       if (touchStartX - touchEndX > 50) {
+//         router.back();
+//       }
+//     };
 
-    document.addEventListener("touchstart", handleTouchStart);
-    document.addEventListener("touchend", handleTouchEnd);
+//     document.addEventListener("touchstart", handleTouchStart);
+//     document.addEventListener("touchend", handleTouchEnd);
 
-    return () => {
-      document.removeEventListener("touchstart", handleTouchStart);
-      document.removeEventListener("touchend", handleTouchEnd);
-    };
-  }, [touchStartX, touchEndX, router]);
-}
+//     return () => {
+//       document.removeEventListener("touchstart", handleTouchStart);
+//       document.removeEventListener("touchend", handleTouchEnd);
+//     };
+//   }, [touchStartX, touchEndX, router]);
+// }
+
+
+// 'use client';
+
+// import { useRouter } from "next/navigation";
+// import { useEffect } from "react";
+
+// export function useSwipeBack() {
+//   const router = useRouter();
+//   let touchStartX = 0;
+
+//   useEffect(() => {
+//     const handleTouchStart = (e) => {
+//       touchStartX = e.touches[0].clientX;
+//     };
+
+//     const handleTouchEnd = (e) => {
+//       const touchEndX = e.changedTouches[0].clientX;
+//       const swipeDistance = touchEndX - touchStartX;
+
+//       // Проверяем, что свайп был справа налево (отрицательный) и больше 50px
+//       if (swipeDistance > 50) {
+//         router.back();
+//       }
+//     };
+
+//     document.addEventListener("touchstart", handleTouchStart);
+//     document.addEventListener("touchend", handleTouchEnd);
+
+//     return () => {
+//       document.removeEventListener("touchstart", handleTouchStart);
+//       document.removeEventListener("touchend", handleTouchEnd);
+//     };
+//   }, [router]);
+// }
