bbb9826e50a71d313a4ecf8da09e6766a4c56934
elfototo <e89617881302@gmail.com>
2025-03-11 19:10:16 +0700

    fixed herosearch

--- app/components/HeroSearch.js
+++ app/components/HeroSearch.js
@@ -29,11 +29,12 @@ export default function Navbar({ search, setSearch, value }) {
                             <input
                                 type="text"
                                 onChange={(e) => setSearch(e.target.value)}
-                                className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-tl-lg rounded-bl-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-opacity-40 focus:ring-blue-300"
+                                className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-opacity-40 
+                                 focus:ring-blue-300"
                                 placeholder={value}
                             />
                         </div>
-                        <button className='px-6 py-2 hover:bg-stone-100 font-medium tracking-wide text-[#333] transition-colors duration-300 transform bg-white rounded-tr-lg rounded-br-lg'>Найти</button>
+                        {/* <button className='px-6 py-2 hover:bg-stone-100 font-medium tracking-wide text-[#333] transition-colors duration-300 transform bg-white rounded-lg'>Найти</button> */}
 
                     </div>
                 </div>
