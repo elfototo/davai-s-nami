5e850fd9e1e2ce3e1dae8673b98f01d795536a57
elfototo <e89617881302@gmail.com>
2025-03-12 12:57:07 +0700

    style text cathegories comp

--- app/components/HeroSearch.js
+++ app/components/HeroSearch.js
@@ -1,8 +1,18 @@
 'use client'
 import Link from 'next/link';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 export default function Navbar({ search, setSearch, value }) {
+    const inputRef = useRef(null);
+
+  const handleClear = () => {
+    setSearch(""); // Очищаем состояние
+    if (inputRef.current) {
+      inputRef.current.value = ""; // Очистка содержимого инпута с помощью рефа
+      inputRef.current.focus(); // Фокус на инпуте после очистки
+    }
+  };
+    
     return (
         <nav className="relative bg-accent-gradient shadow dark:bg-gray-800">
             <div className="max-w-custom-container px-6 py-3 mx-auto">
@@ -31,13 +41,23 @@ export default function Navbar({ search, setSearch, value }) {
                                 onChange={(e) => setSearch(e.target.value)}
                                 onKeyDown={(e) => {
                                     if (e.key === "Enter") {
-                                      e.target.blur(); // Убираем фокус
+                                        e.target.blur(); // Убираем фокус
                                     }
-                                  }}
+                                }}
                                 className="w-full py-2 pl-10 pr-4 text-gray-700 bg-white border rounded-lg focus:border-blue-400 focus:outline-none focus:ring focus:ring-opacity-40 
                                  focus:ring-blue-300"
                                 placeholder={value}
                             />
+                            {/* Крестик для очистки
+                            {search && (
+                                <button
+                                    onClick={handleClear}
+                                    className="absolute inset-y-0 right-0 flex items-center pr-3 text-red-400 hover:text-gray-600"
+                                >
+                                    ✖
+                                </button>
+                            )} */}
+
                         </div>
                         {/* <button className='px-6 py-2 hover:bg-stone-100 font-medium tracking-wide text-[#333] transition-colors duration-300 transform bg-white rounded-lg'>Найти</button> */}
 
