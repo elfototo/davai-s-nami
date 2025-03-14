9edb7d74e2d621713765f01a3f5733f7823a183a
elfototo <e89617881302@gmail.com>
2025-03-14 13:24:16 +0700

    active filter tags and date in mobile

--- app/components/Filtres.js
+++ app/components/Filtres.js
@@ -10,6 +10,8 @@ import isoWeek from 'dayjs/plugin/isoWeek';
 import { useSearchParams } from 'next/navigation';
 import timezone from 'dayjs/plugin/timezone';
 import { categoriesID } from '../data/events';
+import { IoClose } from "react-icons/io5";
+
 
 dayjs.extend(isoWeek);
 dayjs.locale('ru');
@@ -19,7 +21,6 @@ dayjs.extend(timezone);
 
 const Filtres = ({ selectedTags, setSelectedTags, setBgColor, startDate, setStartDate, endDate, setEndDate, isOpen, setIsOpen, }) => {
     const [selectedButton, setSelectedButton] = useState('');
-
 
     const searchParams = useSearchParams();
     const category = searchParams.get('category') || '';
@@ -101,6 +102,32 @@ const Filtres = ({ selectedTags, setSelectedTags, setBgColor, startDate, setStar
         }
 
     };
+    const buttons = [
+        { id: "today", label: "Сегодня", onClick: selectToday },
+        { id: "tomorrow", label: "Завтра", onClick: selectTomorrow },
+        { id: "weekend", label: "Выходные", onClick: selectWeekends },
+
+    ];
+
+    const allButtons = [
+        ...buttons,
+        ...categoriesID.map((category) => ({
+            id: category.id,
+            label: category.category,
+            icon: category.icon,
+            color: category.color,
+            onClick: () => toggleTag(category.category),
+        }))
+    ];
+
+
+    // Сортируем кнопки: активные идут в начало
+    const sortedButtons = [...allButtons].sort((a, b) => {
+        const aActive = selectedButton === a.id || selectedTags.includes(a.label);
+        const bActive = selectedButton === b.id || selectedTags.includes(b.label);
+
+        return bActive - aActive; // Если b активен, он идет выше
+    });
 
     const clearSelection = () => {
         setStartDate(null);
@@ -119,15 +146,48 @@ const Filtres = ({ selectedTags, setSelectedTags, setBgColor, startDate, setStar
         <div className={`${isOpen ? 'bg-white' : 'bg-[#f4f4f9]'} relative rounded-lg lg:bg-white lg:border lg:border-[#D9D9D9] lg:shadow-lg`}>
             <div className={`${isOpen ? 'absolute' : 'block'}`}>
                 <div className='lg:hidden px-2 py-2 flex justify-start -mx-3 overflow-y-auto whitespace-nowrap scroll-hidden'>
-                    <button onClick={toggleFilter} className={` flex mx-3 text-[1rem] items-center justify-center py-1 px-2 bg-[#fff] rounded-md ${!isOpen ? 'block' : 'hidden'}`}>
+                    <button onClick={toggleFilter} className={` flex mx-3 text-[1rem] items-center justify-center py-1 px-2 bg-[#fff] rounded-md ${selectedButton.length > 0 || selectedTags.length > 0 || endDate || startDate ? "bg-pink-400 text-white " : "bg-[#fff]"
+                        } ${!isOpen ? 'block' : 'hidden'}`}>
 
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
                             <path d="M18.75 12.75h1.5a.75.75 0 0 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM12 6a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 6ZM12 18a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 12 18ZM3.75 6.75h1.5a.75.75 0 1 0 0-1.5h-1.5a.75.75 0 0 0 0 1.5ZM5.25 18.75h-1.5a.75.75 0 0 1 0-1.5h1.5a.75.75 0 0 1 0 1.5ZM3 12a.75.75 0 0 1 .75-.75h7.5a.75.75 0 0 1 0 1.5h-7.5A.75.75 0 0 1 3 12ZM9 3.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5ZM12.75 12a2.25 2.25 0 1 1 4.5 0 2.25 2.25 0 0 1-4.5 0ZM9 15.75a2.25 2.25 0 1 0 0 4.5 2.25 2.25 0 0 0 0-4.5Z" />
                         </svg>
                     </button>
 
+                    {/* {selectedButton ?
+                        <button onClick={clearSelection} className={`mr-3 font-medium text-[1rem] text-red-500 items-center justify-center py-1 flex rounded-md ${!isOpen ? 'block' : 'hidden'}`}>
+                            <IoClose size={16} className='mt-[1px] mr-1' />
+                            Сбросить все
+                        </button>
+                        : ''
+                    } */}
 
-                    <button onClick={selectToday} className={`${selectedButton === 'today' ? 'bg-pink-400 text-white transform transition-colors duration-200' : 'bg-[#fff]  '} mx-3 text-[1rem] items-center justify-center py-1 px-2 bg-[#fff] rounded-md ${!isOpen ? 'block' : 'hidden'}`}>
+
+                    {sortedButtons.map((button) => (
+                        <button
+                            key={button.id}
+                            onClick={button.onClick}
+                            className={`mx-3 text-[1rem] items-center justify-center py-1 px-2 rounded-md flex ${selectedButton === button.id || selectedTags.includes(button.label) ? "bg-pink-400 text-white " : "bg-[#fff]"
+                                } ${!isOpen ? 'block' : 'hidden'}`}
+                        >
+                            {button.label}
+                            {selectedButton === button.id || selectedTags.includes(button.label) ? <IoClose size={18} className='mt-[3px] ml-2' />
+                                : ''}
+
+                        </button>
+                    ))}
+
+                    {/* {categoriesID.map((tag) => (
+                        <button
+                            key={tag.id}
+                            onClick={() => toggleTag(tag.category)}
+                            className={`mx-1 my-1 py-1 px-4 text-left rounded-full ${selectedTags.includes(tag.category) ? 'bg-pink-400 text-white' : 'bg-gray-100 hover:bg-gray-200'} ${!isOpen ? 'block' : 'hidden'}`}
+                        >
+                            {tag.category}
+                        </button>
+                    ))} */}
+
+                    {/* <button onClick={selectToday} className={`${selectedButton === 'today' ? 'bg-pink-400 text-white transform transition-colors duration-200' : 'bg-[#fff]  '} mx-3 text-[1rem] items-center justify-center py-1 px-2 bg-[#fff] rounded-md ${!isOpen ? 'block' : 'hidden'}`}>
                         Сегодня
                     </button>
                     <button onClick={selectTomorrow} className={`${selectedButton === 'tomorrow' ? 'bg-pink-400 text-white transform transition-colors duration-200' : 'bg-[#fff]  '} mx-3 text-[1rem] items-center justify-center py-1 px-2 bg-[#fff] rounded-md ${!isOpen ? 'block' : 'hidden'}`}>
@@ -135,10 +195,8 @@ const Filtres = ({ selectedTags, setSelectedTags, setBgColor, startDate, setStar
                     </button>
                     <button onClick={selectWeekends} className={`${selectedButton === 'weekend' ? 'bg-pink-400 text-white transform transition-colors duration-200' : 'bg-[#fff]  '} mx-3 text-[1rem] items-center justify-center py-1 px-2 bg-[#fff] rounded-md ${!isOpen ? 'block' : 'hidden'}`}>
                         Выходные
-                    </button>
-                    <button onClick={clearSelection} className={`bg-[#fff]   mx-3 text-[1rem] items-center justify-center py-1 px-2 rounded-md ${!isOpen ? 'block' : 'hidden'}`}>
-                        Сбросить фильтр
-                    </button>
+                    </button> */}
+
                 </div>
             </div>
 
